81d915e4eb6e9728dc0045aed755d2aed8de1097
Hancer <hancermercedes@gmail.com>
2023-06-17 14:36:19 -0400

    This is the first commit for this project

---
+++ git-expert-app/src/git.expert.app.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import AddCategory from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+export function GitExpertApp(){
+
+    const [categories, setCategories] = useState(['One Punch']);
+    
+    const onAddCategory = (newCategory) =>{
+
+      if(categories.includes(newCategory)) return;
+
+
+        setCategories([newCategory, ...categories]);
+
+    }
+
+    return(
+        <>
+        <h1>GifExpertApp</h1>
+
+        <AddCategory 
+           onNewCategory = {onAddCategory}
+         />
+
+
+          {
+            categories.map((category) => (
+              <GifGrid key={ category } category={ category }/>
+            ))
+          }
+
+        </>
+    )
+}
